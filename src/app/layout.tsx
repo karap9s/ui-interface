523d3bce7bcb7e7c92dde5e4e700e9be6ee4fccf
Daniil Sharenkov <Milkiy174@gmail.com>
2025-08-05 01:27:52 +0300

    fix: hydratation error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className="min-h-screen">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Theme>
